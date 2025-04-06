87dacf7afb66a2e4bb5aebf6082640c098cc4201
Adarsh Kumar Singh <138415747+GitadarshSingh@users.noreply.github.com>
2025-04-06 22:39:03 +0530

    LMS Activity, Next Permutation

---
+++ src/Problems/NextPermutation.java
@@ -0,0 +1,55 @@
+package Problems;
+import java.util.Scanner;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        nextPermutation(arr);
+
+        System.out.println("Next Permutation:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void nextPermutation(int[] arr) {
+        int n = arr.length;
+        int i = n - 2;
+
+        while (i >= 0 && arr[i] >= arr[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+            while (arr[j] <= arr[i]) {
+                j--;
+            }
+            swap(arr, i, j);
+        }
+
+        reverse(arr, i + 1, n - 1);
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+}
+
